4845c5456aa2e989d96604e564170674d565085b
Casper Gamborg <Gamborgc@gmail.com>
2026-03-23 12:44:42 +0100

    logger

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(logger);
+
 router.get("/", (req, res) => {
   res.send("Users list");
 });
@@ -12,8 +14,27 @@ router.post("/", (req, res) => {
   res.send("Create user");
 });
 
-router.get("/:id", (req, res) => {
-  res.send(`Get User With ID ${req.params.id}`);
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`);
+  });
+
+const users = [{ name: "Kyle" }, { name: "Casper" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
 });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
