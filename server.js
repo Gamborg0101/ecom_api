fa7676dd9d644ea956bfa6ba9544be37b506f227
Casper Gamborg <Gamborgc@gmail.com>
2026-03-23 12:09:49 +0100

    implemented ejs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   console.log("here");
-  res.send("Hi");
+  res.render("index", { text: "World" });
 });
 
 app.listen(3000);
